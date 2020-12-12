c5badf9b4c9300fef930cfff07b0106803a2ef49
nnsnico <yuji.jampan.blue@gmail.com>
2020-12-12 11:49:18 +0900

    Add watch mode by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const path = require("path");
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
   mode: "development",
 
   // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: "./src/index.tsx",
-
+  entry: {
+    main: "./src/index.tsx",
+  },
   module: {
     rules: [
       {
         // 拡張子 .ts の場合
         test: /\.ts$|tsx/,
+        include: path.join(__dirname, "src"),
         // TypeScript をコンパイルする
-        use: "ts-loader",
+        use: [
+          isDevelopment && {
+            loader: "babel-loader",
+            options: { plugins: ["react-refresh/babel"] },
+          },
+          {
+            loader: "ts-loader",
+            options: { transpileOnly: true },
+          },
+        ].filter(Boolean),
       },
     ],
   },
+  plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+    new HtmlWebpackPlugin(),
+  ].filter(Boolean),
   // import 文で .ts ファイルを解決するため
   // これを定義しないと import 文で拡張子を書く必要が生まれる。
   // フロントエンドの開発では拡張子を省略することが多いので、
@@ -25,4 +47,3 @@ module.exports = {
     extensions: [".ts", ".js", ".tsx", ".jsx"],
   },
 };
-
