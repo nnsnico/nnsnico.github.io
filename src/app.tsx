0ce533f1618a65e32b64b08e3f792848f0988eb4
Arakawa <t_arakawa@fancs.com>
2020-12-12 10:45:53 +0900

    add Header,Footer and logo.svg with chakra-ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import Footer from "./components/footer";
+import Navigation from "./components/navigation";
 
 const App: React.FC = () => {
-  return <div>hogehogefugao</div>;
+    return <div><Navigation/><Footer/></div>;
 };
 
 export default App;
