7b7bdf1da5dcf288cd04ae3a524dd15fe8c6f18c
nnsnico <yuji.jampan.blue@gmail.com>
2020-12-30 16:11:35 +0900

    Add type of keycap size

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,16 @@
-export type KeyCapId = string;
+export const keycapSize = [
+  '1U',
+  '1.25U',
+  '1.5U',
+  '1.75U',
+  '2U',
+  '2.25U',
+  '2.75U',
+  '5U',
+  '6U',
+  '6.25U',
+  '6.5U',
+  'ISOEnter',
+] as const;
+
+export type KeycapSize = typeof keycapSize[number];
