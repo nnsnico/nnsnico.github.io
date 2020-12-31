cecc3c2cd8e287215a5553208d5270f776f1d848
nnsnico <yuji.jampan.blue@gmail.com>
2021-01-01 02:45:40 +0900

    Replace to keyboard component by fp-ts

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -15,5 +15,3 @@ export const { insertKeyCap, updateKeyCapPosition } = keyboardSlice.actions;
 
 // store
 export const store = createStore(reducers);
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
