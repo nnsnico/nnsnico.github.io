9c189367abeb10f4e874eb24dd4029ac251cdf85
arabbit <tsupo222@gmail.com>
2021-04-04 10:35:54 +0900

    ISOEnter can drop at ISOEnter_TOP and ISOEnter_BOTTOM keyframes

--- src/components/atomic/keyframe.tsx
+++ src/components/atomic/keyframe.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { PayloadAction } from '@reduxjs/toolkit';
+import React, { Dispatch } from 'react';
 import { useDrop } from 'react-dnd';
 import { useDispatch } from 'react-redux';
 
 import { convertNumberFromUnit } from '../../keycapSize';
 import { updateKeycap } from '../../reducer';
+import { UpdateKeycapPayload } from '../../reducer/keyboard';
 import { DragItem, KeycapSize, Position } from '../../types';
 
 export interface KeyFrameProps {
@@ -21,16 +23,34 @@ const KeyFrame: React.FC<KeyFrameProps> = (props: KeyFrameProps) => {
     canDrop: () => true,
     drop: (_, monitor) => {
       const item = monitor.getItem() as DragItem;
+
       if (props.size == item.size) {
-        dispatch(
-          updateKeycap({
-            size: props.size,
-            position: props.position,
-            usedKey: {
-              id: item._key,
-              selected: false,
-            },
-          })
+        if (props.size.match(/ISOEnter_(TOP|BOTTOM)/)) {
+          //ISOEnter
+          ISOEnter(
+            dispatch,
+            props.position,
+            props.size as 'ISOEnter_TOP' | 'ISOEnter_BOTTOM'
+          );
+          return;
+        } else {
+          return dispatch(
+            updateKeycap({
+              size: props.size,
+              position: props.position,
+              usedKey: {
+                id: item._key,
+                selected: false,
+              },
+            })
+          );
+        }
+      } else if (props.size.match(/ISOEnter_(TOP|BOTTOM)/)) {
+        //ISOEnter
+        ISOEnter(
+          dispatch,
+          props.position,
+          props.size as 'ISOEnter_TOP' | 'ISOEnter_BOTTOM'
         );
       }
     },
@@ -48,5 +68,61 @@ const KeyFrame: React.FC<KeyFrameProps> = (props: KeyFrameProps) => {
       {props.size}
     </div>
   );
+
+  function ISOEnter(
+    dispatch: Dispatch<PayloadAction<UpdateKeycapPayload>>,
+    position: Position,
+    ISOEnterSize: 'ISOEnter_TOP' | 'ISOEnter_BOTTOM'
+  ): void {
+    if (ISOEnterSize.includes('TOP')) {
+      dispatch(
+        updateKeycap({
+          size: 'ISOEnter_TOP',
+          position: position,
+          usedKey: {
+            id: 'ISOEnter_TOP',
+            selected: false,
+          },
+        })
+      );
+      dispatch(
+        updateKeycap({
+          size: 'ISOEnter_BOTTOM',
+          position: {
+            x: position.x,
+            y: position.y + 1,
+          },
+          usedKey: {
+            id: 'ISOEnter_BOTTOM',
+            selected: false,
+          },
+        })
+      );
+    } else {
+      dispatch(
+        updateKeycap({
+          size: 'ISOEnter_TOP',
+          position: {
+            x: position.x,
+            y: position.y - 1,
+          },
+          usedKey: {
+            id: 'ISOEnter_TOP',
+            selected: false,
+          },
+        })
+      );
+      dispatch(
+        updateKeycap({
+          size: 'ISOEnter_BOTTOM',
+          position: position,
+          usedKey: {
+            id: 'ISOEnter_BOTTOM',
+            selected: false,
+          },
+        })
+      );
+    }
+  }
 };
 export default KeyFrame;
