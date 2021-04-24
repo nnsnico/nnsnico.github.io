cbf9c1f90d809254ce7414d587e5196c3b46c6d3
nnsnico <yuji.jampan.blue@gmail.com>
2021-04-24 09:37:50 +0900

    Remove counter reducer

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,14 +1,9 @@
 import { Button, Flex } from '@chakra-ui/react';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
-import { decrement, increment } from '../reducer';
-import { RootState } from '../types';
 import Logo from './logo';
 
 const Navigation: React.FC = () => {
-  const count = useSelector((state: RootState) => state.counter);
-  const dispatch = useDispatch();
   return (
     <Flex
       as="nav"
@@ -20,12 +15,6 @@ const Navigation: React.FC = () => {
       boxShadow="xl"
       style={{ zIndex: 1000 }}>
       <Logo />
-      <div>
-        <span>{count}</span>
-        {/* eslint-disable @typescript-eslint/explicit-function-return-type */}
-        <Button onClick={() => dispatch(increment())}>+</Button>
-        <Button onClick={() => dispatch(decrement())}>-</Button>
-      </div>
       <Button backgroundColor="#4FC3F7" color="white" size="sm">
         使い方
       </Button>
--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,18 +1,15 @@
 import { combineReducers, createStore } from '@reduxjs/toolkit';
 
-import counterSlice from './counter';
 import keyboardSlice from './keyboard';
 import pcbSlice from './pcb';
 
 // reducers
 const reducers = combineReducers({
-  counter: counterSlice.reducer,
   keyboard: keyboardSlice.reducer,
   pcb: pcbSlice.reducer,
 });
 
 // actions
-export const { increment, decrement } = counterSlice.actions;
 export const {
   initKeyBoard,
   removeKeycap,
