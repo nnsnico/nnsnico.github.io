e85c500fa50f9831999766d4caa574fa9bf79e89
nnsnico <y_toyama@fancs.com>
2022-09-13 21:19:57 +0900

    Tweak 404 page design

--- src/components/notFound.tsx
+++ src/components/notFound.tsx
@@ -1,22 +1,18 @@
-import {
-  Center,
-  Flex,
-  Heading,
-  HStack,
-  Link as LinkText,
-  VStack,
-} from '@chakra-ui/react';
+import { Flex, Heading, Link as LinkText, } from '@chakra-ui/react';
 import { RouteComponentProps } from '@reach/router';
 import React from 'react';
 import { Link } from 'gatsby';
+import Container from './layouts/container';
 
 const NotFound: React.FC<RouteComponentProps> = () => (
-  <Flex direction="column" align="center">
-    <Heading>404: Not Found</Heading>
-    <LinkText>
-      <Link to="/">Back to home page</Link>
-    </LinkText>
-  </Flex>
+  <Container>
+    <Flex direction="column" align="center">
+      <Heading>404: Not Found ¯\_(´ワ｀ )_/¯</Heading>
+      <LinkText>
+        <Link to="/">Back to website</Link>
+      </LinkText>
+    </Flex>
+  </Container>
 );
 
 export default NotFound;
